23365f024d015d06e0dba9a8e7a9cf9e9284768d
cacero95 <cacero95@hotmail.com>
2020-01-15 17:17:45 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuMJX0CW9cuLK_MbzAPVz0ViCB-qmdkSY",
+  authDomain: "hemodosis-sam.firebaseapp.com",
+  databaseURL: "https://hemodosis-sam.firebaseio.com",
+  projectId: "hemodosis-sam",
+  storageBucket: "",
+  messagingSenderId: "832387172904",
+  appId: "1:832387172904:web:1f08c322c1d5e7a837431d"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+      IonicModule.forRoot(),
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireDatabaseModule,
+      AngularFireAuthModule,
+      AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
